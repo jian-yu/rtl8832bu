98bfc93507d36866a9872785cd2141af62452ec3
Jianyu Zhuang <xzjianyu@gmail.com>
2025-03-14 14:29:07 +0800

    init

---
+++ phl/phl_git_info.h
@@ -0,0 +1,43 @@
+/******************************************************************************
+ *
+ * Copyright(c) 2020 Realtek Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * The full GNU General Public License is included in this distribution in the
+ * file called LICENSE.
+ *
+ * Contact Information:
+ * wlanfae <wlanfae@realtek.com>
+ * Realtek Corporation, No. 2, Innovation Road II, Hsinchu Science Park,
+ * Hsinchu 300, Taiwan.
+ *
+ * Larry Finger <Larry.Finger@lwfinger.net>
+ *
+ *****************************************************************************/
+#ifndef __PHL_GIT_INFO_H__
+#define __PHL_GIT_INFO_H__
+ /*@--------------------------[Define] ---------------------------------------*/
+
+#define RTK_CORE_SHA1   "c327097e0d437203b579f5ca99b7fe957b005cd9"
+#define RTK_PHL_SHA1    "869eda09d8de5e84bcf05e0c3951976681574201"
+#define RTK_HALMAC_SHA1 "1019b3b7fb280d4a2d4b737b6c5212c47d648ba1"
+#define RTK_HALBB_SHA1  "0f3e25a32c1f83308e40cbee9a2946a9df722426"
+#define RTK_HALRF_SHA1  "ee0c0758272ef0a26f2fd63df25b8ec7389bbefd"
+#define RTK_BTC_SHA1    "fd139fff0d6f2838930470278bf35dfb6df800b7"
+
+#define RTK_CORE_TAGINFO     "v1.15.11-15-gc327097e0"
+#define RTK_PHL_TAGINFO      "PHL_1_15_62_0100-10-g869eda09d"
+#define RTK_HALMAC_TAGINFO   "HALMAC_0_25_61_0-11-g1019b3b7f"
+#define RTK_HALBB_TAGINFO    "HALBB_025_033_07-10-g0f3e25a3"
+#define RTK_HALRF_TAGINFO    "HALRF_025_00_039-1-gee0c075"
+#define RTK_BTC_TAGINFO      "HALBTC_025_030-0-gfd139ff"
+
+#endif /* __PHL_GIT_INFO_H__ */
